3897818dbb2342f7ecc470866e25df1c318843c9
khe0070 <capstone0010@gmail.com>
2022-10-27 16:42:53 +0800

    second commit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Col, Row, Container, Image } from 'react-bootstrap';
 
-import HIMG from '../images/hero-desktop.png';
+import Programmer from '../images/programmer_vector.jpg';
 import Logo from '../images/logo9.png';
+import JS from '../images/svg/javascript.svg';
+import ReactJS from '../images/svg/react.svg';
+import Node from '../images/svg/node.svg';
+import Next from '../images/svg/nextjs2.svg';
+
 const HeroSection = () => {
   return (
     <>
@@ -16,18 +21,38 @@ const HeroSection = () => {
               <Col md={6} xs={12} className='hero_text_section'>
                 <div className='border_text'>
                   <div className='text_container'>
-                    <h4>
-                      Hello, I am <strong>Kenneth</strong>
-                    </h4>
+                    <div className='welcome'>
+                      <strong>WELCOME</strong>
+                    </div>
+
                     <div className='web_container'>
                       <h2>Software Developer</h2>
                       <h3>at your service</h3>
                     </div>
                     <p>
-                      - Over the course of 3 years I've worked with startup
+                      My name is <strong>Kenneth</strong>, I'm a front-end
+                      developer based in the Philippines.
+                    </p>
+
+                    <p>
+                      Over the course of 3 years I've worked with startup
                       companies by assisting them to reach their full potential
                       & prosper to their utmost capabilities.
                     </p>
+                    <div className='section2_img_container'>
+                      <span>
+                        <Image src={JS} />
+                      </span>
+                      <span>
+                        <Image src={ReactJS} />
+                      </span>
+                      <span>
+                        <Image src={Next} />
+                      </span>
+                      <span>
+                        <Image src={Node} />
+                      </span>
+                    </div>
                     <span>
                       <i class='fa-solid fa-list-check'></i>View Projects
                     </span>
